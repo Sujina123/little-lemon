4d45296f5266ccd4c4efabf1ae5bc82d9cf54167
Sujina123 <61376347+Sujina123@users.noreply.github.com>
2024-06-22 19:51:20 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,19 @@
+import React from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+
+      <Main />
+
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
 
